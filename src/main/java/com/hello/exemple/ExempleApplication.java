cd01855612fff4e18aef766519a2472466fa58b9
Alaoui <ismaili.alaoui.1997@gmail.com>
2024-05-26 13:15:27 +0200

    Initial commit

---
+++ src/main/java/com/hello/exemple/ExempleApplication.java
@@ -0,0 +1,15 @@
+package com.hello.exemple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExempleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExempleApplication.class, args);
+        System.out.println("Hello ALAOUII!");
+
+    }
+
+}
